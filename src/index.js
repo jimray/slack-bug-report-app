7d6a8da03cc8a979a1b15f775bae811af2a94b52
Jim Ray <jray@slack-corp.com>
2017-10-12 15:58:32 -0700

    Just adding some code comments

--- src/index.js
+++ src/index.js
@@ -15,20 +15,29 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/*
+ * Default endpoint
+ */
 app.get('/', (req, res) => {
   res.send('<h2>The Dialog Slash Command app is running</h2> <p>Follow the' +
   ' instructions in the README to configure the Slack App and your environment variables.</p>');
 });
 
+/*
+ * Endpoint to handle slash commands from Slack
+ * Echos submitted text back to user
+ */
 app.post('/slack/events/commands', (req, res) => {
   const { token, text, response_url } = req.body;
 
   if (token === process.env.SLACK_VERIFICATION_TOKEN) {
-    // echo the text back to Slack
     axios.post(response_url, text);
   } else { res.sendStatus(500); }
 });
 
+/*
+ * Start the express server
+ */
 app.listen(app.get('port'), () => {
   console.log(`App listening on port ${app.get('port')}!`);
 });
