6ecda5f904c01f50e8fb4a51fcb76346bee4f578
Jim Ray <jray@slack-corp.com>
2017-10-12 15:51:00 -0700

    Add a little more documentation

--- src/index.js
+++ src/index.js
@@ -15,11 +15,17 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/*
+ * Default endpoint
+ */
 app.get('/', (req, res) => {
   res.send('<h2>The Dialog Slash Command app is running</h2> <p>Follow the' +
   ' instructions in the README to configure the Slack App and your environment variables.</p>');
 });
 
+/*
+ * Start the express server
+ */
 app.listen(app.get('port'), () => {
   console.log(`App listening on port ${app.get('port')}!`);
 });
